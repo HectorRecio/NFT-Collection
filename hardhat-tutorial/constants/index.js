d1ec09161a9667cb8c393c39ae1225f8c4dd51f8
The Red Dime LLC <hector.recio.do@gmail.com>
2022-05-11 11:55:28 +0200

    First Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x86C70e48859F8663C34Ca40adbAeA9bf4EEA5f12";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-dapp-hectorrecio.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
